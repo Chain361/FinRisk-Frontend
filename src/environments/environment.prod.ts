4a470e3a7891a86550b2997ee7f1a3a075f6ae50
Stang <stang.wiramol@gmail.com>
2026-07-23 23:06:30 +0700

    fix conflict

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
-<<<<<<< HEAD
   apiBaseUrl: 'https://finrisk-backend.vercel.app',
-=======
-  apiBaseUrl: 'https://finrisk-backend-mockup.vercel.app',
->>>>>>> 4c65753 (chore(mockup): point API at finrisk-backend-mockup.vercel.app (public prod URL))
 };
